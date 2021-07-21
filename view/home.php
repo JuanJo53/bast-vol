428b563bacc8030ae7289d6c62f4354de0c0cf78
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-07-21 17:25:27 -0400

    home page done

--- view/home.php
+++ view/home.php
@@ -15,7 +15,7 @@
 		<div class="container">
             <?php
                 session_start();    
-                echo "<h4 style='color:white;margin-right:10px;'>Hola ". $_SESSION['USUARIO']."</h4>";
+                echo "<h4 style='color:white;margin-right:10px;'>¡Hola ". $_SESSION['USUARIO']."!</h4>";
             ?>
 			<a class="navbar-brand" href="#">BAST-VOL</a>
 			<button
@@ -30,9 +30,9 @@
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbar">
-				<ul class="navbar-nav ml-auto">
+				<ul class="navbar-nav ml-auto ms-5">
 					<li class="nav-item">
-						<a class="nav-link" href="#main">Inicio</a>
+						<a class="nav-link active" href="#">Inicio</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="staticPages/proposito.html">Articulos</a>
@@ -43,9 +43,16 @@
 					<li class="nav-item">
 						<a class="nav-link" href="staticPages/proposito.html">Ventas</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="staticPages/proposito.html">Usuarios</a>
-					</li>
+                    <?php
+                        if($_SESSION['TIPO']=='admin'){
+                            echo "<li class='nav-item'>
+                                    <a class='nav-link' href='staticPages/proposito.html'>Proveedores</a>
+                                </li>";
+                            echo "<li class='nav-item'>
+                                    <a class='nav-link' href='staticPages/proposito.html'>Usuarios</a>
+                                </li>";
+                        }
+                    ?>
 					<!-- <li class="nav-item dropdown">
 						<a
 							class="nav-link dropdown-toggle"
@@ -65,111 +72,88 @@
 					</li> -->
 				</ul>
                 <form method="post" action="../controller/logout.php">
-                    <button class="btn btn-danger text-md-" type="submit">Cerrar Sesion</button>
+                    <button class="btn btn-danger text-md- ms-5" type="submit">Cerrar Sesion</button>
                 </form>
 			</div>
 		</div>
 	</nav>
 	<!--header-->
-	<div class="container-fluid">
+    <div class="container">
 		<div class="row">
-			<nav class="col-md-2 d-none d-md-block bg-dark">
-				<div id="alimentoDiario" class="position-sticky sticky-top text-center text-white mt-4 margin:2rem">
-					<br />
-					<br />
-					<h5>
-						ALIMENTO DIARIO
-					</h5>
-					<h3>02</h3>
-					<span>Lunes</span>
-					<p>
-						La paga del pecado es muerte, mas la dádiva de Dios es vida eterna en Cristo Jesús Señor nuestro.
-					</p>
-					<span>
-						Romanos 6:23
-					</span>
-				</div>
-			</nav>
-			<!--Noticias Todas-->
-			<section id="noticias-todas" class="mt-4 col-md-10">
-				<div class="container">
-					<div class="row">
-						<div class="col text-center text-uppercase">
-							<small>Mira las</small>
-							<h2>Noticias</h2>
-						</div>
-					</div>
-					<div class="row">
-					<?php
-                        require 'conexionBD.php';
-                        $data = "SELECT * FROM noticias";
-                        $dataResult = $conn->query($data);
-                        if($dataResult->num_rows>0){
-                            while($row=$dataResult->fetch_array()){?>
-                                <div class="col-sm-6 col-lg-6 col-md-2 mb-4 mt-4">
-                                    <div data-target="codigo" id="<?php echo $row['cod_noticia'];?>" class="card">
-                                        <div class="card-body">
-                                            <h5 class="card-title"><strong data-target="titulo" id="titulo<?php echo $row['cod_noticia'];?>"><?php echo $row['titulo'];?></strong></h5>
-                                            <div class="badge">
-                                                <?php 
-                                                    $categoria=$row['cod_categoria'];
-                                                    $cat = "SELECT * FROM categorias_noticias WHERE cod_categoria='$categoria'";
-                                                    $dataResultCat = $conn->query($cat);
-                                                    if($dataResultCat->num_rows>0){
-                                                        while($rowCat=$dataResultCat->fetch_array()){
-                                                            switch($rowCat['categoria']){
-                                                                case 'Profesias':
-                                                                    echo "<span class='badge badge-info' data-target='categoria' id='categoria".$row['cod_noticia']."'>".$rowCat['categoria']."</span>";
-                                                                    break;
-                                                                case 'Desastres Naturales':
-                                                                    echo "<span class='badge badge-secondary' data-target='categoria' id='categoria".$row['cod_noticia']."'>".$rowCat['categoria']."</span>";
-                                                                    break;
-                                                                case 'Israel':
-                                                                    echo "<span class='badge badge-primary' data-target='categoria' id='categoria".$row['cod_noticia']."'>".$rowCat['categoria']."</span>";
-                                                                    break;
-                                                                case 'Ciencia y Tecnologia':
-                                                                    echo "<span class='badge badge-success' data-target='categoria' id='categoria".$row['cod_noticia']."'>".$rowCat['categoria']."</span>";
-                                                                    break;
-                                                            }
-                                                        }
-                                                    }
-                                                ?>
-                                            </div>
-                                            <p class="card-text" data-target="contenido" id="contenido<?php echo $row['cod_noticia'];?>">
-                                            <?php echo $row['contenido'];?>
-                                            </p>
-                                            <div class="d-flex bd-highlight mb-3">    
-                                                <a target="_blank" href="<?php echo $row['enlace_noticia'];?>" data-target="enlace" id="enlace<?php echo $row['cod_noticia'];?>" class="mr-auto p-2 bd-highlight btn btn-outline-primary"
-                                                    >Ver Noticia Original</a
-                                                >                                        
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                    <?php
-                            }
-                        }
-                    ?>
-					</div>
-				</div>
-			</section>
-			<!--Noticias Todas-->
+			<div class="col text-center text-uppercase">
+                <div class="home-title">
+                    <h5>Inicio</h5>
+                    <h1>¡Comienza a Gestionar!</h1>
+                </div>
+			</div>
 		</div>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+                <div class="card">
+                <svg class="card-img-top" xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+                </svg>
+                <div class="card-body">
+                    <h5 class="card-title"><a href="products.php">Articulos</a></h5>
+                    <p class="card-text">Podrás gestionar tus productos y articulos en tu tienda de la mejor forma. Aqui podrás agregar, editar, eliminar articulos, además de poder visualizar todos tus productos con un detalle particular.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <svg class="card-img-top" xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                </svg>
+                <div class="card-body">
+                    <h5 class="card-title"><a href="clients.php">Clientes</a></h5>
+                    <p class="card-text">Podrás gestionar los clientes registrados en tu tienda de la mejor forma. Aqui podrás agregar, editar, eliminar clientes, además de poder visualizar a todos tus clientes a detalle.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <svg class="card-img-top" xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                </svg>
+                <div class="card-body">
+                    <h5 class="card-title"><a href="sales.php">Ventas</a></h5>
+                    <p class="card-text">Podrás administrar las ventas en tu tienda de la mejor forma. Aqui podrás agregar, editar, eliminar ventas, además de poder visualizar a todas tus ventas a detalle.</p>
+                </div>
+                </div>
+            </div>
+            <?php
+                if($_SESSION['TIPO']=='admin'){
+                    echo "<div class='col'>
+                            <div class='card'>
+                            <svg class='card-img-top' xmlns='http://www.w3.org/2000/svg' width='150' height='150' fill='currentColor' class='bi bi-truck' viewBox='0 0 16 16'>
+                            <path d='M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'/>
+                            </svg>
+                            <div class='card-body'>
+                                <h5 class='card-title'><a href='providers.php'>Proveedores</a></h5>
+                                <span class='badge bg-warning text-dark'>¡FUNCIONALIDAD SOLO PARA ADMINISTRADORES! </span>
+                                <p class='card-text'>Podrás gestionar los proveedores registrados en tu tienda de la mejor forma. Aqui podrás agregar, editar, eliminar proveedores, además de poder visualizar a todos tus proveedores a detalle.</p>
+                            </div>
+                            </div>
+                        </div>";
+                    echo "<div class='col'>
+                            <div class='card'>
+                            <svg class='card-img-top' xmlns='http://www.w3.org/2000/svg' width='150' height='150' fill='currentColor' class='bi bi-person-badge' viewBox='0 0 16 16'>
+                            <path d='M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z'/>
+                            <path d='M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z'/>
+                            </svg>
+                            <div class='card-body'>
+                                <h5 class='card-title'><a href='usaers.php'>Usuarios</a></h5>                                
+                                <span class='badge bg-warning text-dark'>¡FUNCIONALIDAD SOLO PARA ADMINISTRADORES! </span>
+                                <p class='card-text'>Podrás gestionar los otros usuarios registrados en tu tienda de la mejor forma. Aqui podrás agregar, editar, eliminar usuarios, además de poder visualizar a todos tus usuarios a detalle.</p>
+                            </div>
+                            </div>
+                        </div>";
+                }                            
+            ?>
+        </div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script>
-		$(function () {
-			$('[data-toggle="tooltip"').tooltip();
-		});
-		$(function () {
-			$(".sticky-content").sticky({
-				topSpacing: 50,
-				zIndex: 2,
-				stopper: "#footer"
-			});
-		});
-	</script>
 </body>
 </html>
