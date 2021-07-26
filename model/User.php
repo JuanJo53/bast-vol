8e96b7383f681cf9d67a0608a927dff6f857ff54
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-07-26 01:56:16 -0400

    user update done, need to show user details

--- model/User.php
+++ model/User.php
@@ -16,23 +16,24 @@
 			}
 		}
 		public function getUserById($id){
-			$sql = "SELECT * FROM usuario where nombre = '$user' and contraseña = '$pass'";
+			$sql = "SELECT * FROM usuario where USR_ID = '$id'";
 			$result = $this->connect()->query($sql);
-			$numrows = $result->num_rows;
-			if($numrows == 1){
-				return true;
+			if($result->num_rows>0){
+				return $result;
 			}else{
 				return false;
 			}
 		}
 
-		public function updateUser($id){
-			$sql3 = "SELECT * FROM user where nombre = '$user' and contraseña = '$pass' and identificador = 'MOD'";
-			$result3 = $this->connect()->query($sql3);
-			//$numrows3 = $result3->num_rows;
-			$row3 = $result3->fetch_assoc();
-			$name3=$row3['iduser'];
-			return $name3;
+		public function updateUser($id,$name,$lastname,$phone,$email,$username,$password,$type){
+			$sql = "UPDATE usuario SET USR_NOMBRES='$name',USR_APELLIDOS='$lastname',USR_TELEFONO='$phone',USR_CORREO='$email',USR_USER='$username',USR_PASSWORD='$password',USR_TIPO='$type'
+					WHERE USR_ID='$id'";
+			$result = $this->connect();
+			if(mysqli_query($result, $sql)){
+				return 'Exito!';
+			}else{
+				return "Error: " . $sql . "<br>" . mysqli_error($result);
+			}
 		}
 		public function deleteUser($id){
 			$sql4 = "SELECT * FROM user where nombre = '$user' and contraseña = '$pass' and identificador = 'PRO'";
--- view/users.php
+++ view/users.php
@@ -8,6 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/main.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).on("click", ".openEditModal", function () {
+            var id = $(this).data('id');
+            $(".idInput #usr_idE").val( id );
+            $.post('../controller/users/updateUser.php', {"usr_idEdit": id});
+        });
+    </script>
 </head>
 <body data-spy="scroll" data-target="#navbar" data-offset="56">
 	<!--Header-->
@@ -116,7 +127,7 @@
                     </thead>
                     <tbody>
                         <?php
-                            include '../controller/users/getUsers.php';
+                            include_once '../controller/users/getUsers.php';
                             echo showUsers();
                         ?>
                     </tbody>
@@ -136,7 +147,7 @@
             <form method='post' action="../controller/users/newUser.php">
             <div class="modal-body">
                     <div class="mb-3">
-                        <label for="usr_names" class="col-form-label">Nombres:</label>
+                        <label for="usr_name" class="col-form-label">Nombres:</label>
                         <input type="text" class="form-control" id="usr_name" name="usr_name" placeholder="Nuevo Usuario" required>
                     </div>
                     <div class="mb-3">
@@ -186,34 +197,42 @@
                 <h5 class="modal-title" id="exampleModalLabel">Detalles del Cliente</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form method='put'>
+            <form method='post' action="../controller/users/updateUser.php">
             <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="usr_names" class="col-form-label">Nombres:</label>
-                        <input type="text" class="form-control" id="usr_name" name="usr_name" placeholder="Nuevo Usuario" required>
+                    <div class="mb-3 idInput">                        
+                        <label for="usr_idE" class="col-form-label">ID:</label>
+                        <input type="text" class="form-control" id="usr_idE" name="usr_idE" readonly>
                     </div>
                     <div class="mb-3">
-                        <label for="usr_last_names" class="col-form-label">Apellidos del Usuario:</label>
-                        <input type="text" class="form-control" id="usr_last_names" name="usr_last_names" placeholder="Perez" required>
+                        <label for="usr_namee" class="col-form-label">Nombres:</label>
+                        <input type="text" class="form-control" id="usr_namee" name="usr_namee" placeholder="Nuevo Usuario" required>
                     </div>
                     <div class="mb-3">
-                        <label for="usr_phone" class="col-form-label">Telefono del Usuario:</label>
-                        <input type="number" class="form-control" id="usr_phone" name="usr_phone" placeholder="22222222" required>
+                        <label for="usr_last_namese" class="col-form-label">Apellidos del Usuario:</label>
+                        <input type="text" class="form-control" id="usr_last_namese" name="usr_last_namese" placeholder="Perez" required>
                     </div>
                     <div class="mb-3">
-                        <label for="usr_email" class="col-form-label">Correo del Usuario:</label>
-                        <input type="text" class="form-control" id="usr_email" name="usr_email" placeholder="cliente@cliente.com" required>
+                        <label for="usr_phonee" class="col-form-label">Telefono del Usuario:</label>
+                        <input type="number" class="form-control" id="usr_phonee" name="usr_phonee" placeholder="22222222" required>
                     </div>
                     <div class="mb-3">
-                        <label for="usr_username" class="col-form-label">Nombre de Usuario:</label>
-                        <input type="text" class="form-control" id="usr_username" name="usr_username" placeholder="cliente@cliente.com" required>
+                        <label for="usr_emaile" class="col-form-label">Correo del Usuario:</label>
+                        <input type="text" class="form-control" id="usr_emaile" name="usr_emaile" placeholder="cliente@cliente.com" required>
                     </div>
                     <div class="mb-3">
-                        <label for="usr_type" class="col-form-label">Tipo:</label>
-                        <select class="form-select" aria-label="Category select" id="usr_type" name="usr_type" required> 
+                        <label for="usr_usernamee" class="col-form-label">Nombre de Usuario:</label>
+                        <input type="text" class="form-control" id="usr_usernamee" name="usr_usernamee" placeholder="algunusuario" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="usr_passworde" class="col-form-label">Contraseña:</label>
+                        <input type="password" class="form-control" id="usr_passworde" name="usr_passworde" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="usr_typee" class="col-form-label">Tipo:</label>
+                        <select class="form-select" aria-label="Category select" id="usr_typee" name="usr_typee" required> 
                             <option selected disabled>Ninguna</option>
-                            <option value="1">Administrador</option>
-                            <option value="2">Empleado</option>
+                            <option value="admin">Administrador</option>
+                            <option value="empleado">Empleado</option>
                         </select>
                     </div>
                 </div>
@@ -247,9 +266,6 @@
     </div>
     <!-- Delete User Modal -->
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 </html>
